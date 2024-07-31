d3dcc74ec11c3835a4c25aab85020f34eb4b9387
idamodar9 <damodarvakamulla9@gmail.com>
2024-07-31 12:58:11 +0530

    Swapping program

---
+++ Swapping.java
@@ -0,0 +1,16 @@
+class Swapping
+{
+public static void main (String[]args)
+{
+  int a = 50,b=70,c=80,d;
+ d=a;
+ a=b;
+ b=c;
+ c=d;
+
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+}
+}
+

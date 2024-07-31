e4355edead96ab7de7c3061db4eecfbe22ebfdd2
idamodar9 <damodarvakamulla9@gmail.com>
2024-07-31 12:31:57 +0530

    message

---
+++ a.java
@@ -0,0 +1,17 @@
+class a
+{
+	public static void main(String[] args)
+	{
+	 int a = 5912;
+	 int b = a%100;
+		if(b%3==0)
+		{
+		System.out.println(b+" is divisible b 3");
+		}
+		
+		else
+		{
+		System.out.println(b+" is not divisible by 3");
+		}
+	}
+}

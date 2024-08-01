420170799dd6369776cde8f1be7465ecc8680de0
ramanjaneyulureddy400@gmail.com <ramanjaneyulureddy400@gmail.com>
2024-08-01 19:05:15 +0530

    ram

--- Attendence.java
+++ Attendence.java
@@ -2,8 +2,8 @@ class Attendence
 {
 	public static void main(String[] args)
 	{
-	  int class_held = 200000;
-	  double class_attend = 10011;
+	  int class_held = 20000;
+	  double class_attend = 1011;
 	  double percentage = class_attend/class_held*100;
 	  System.out.println(percentage);
 	

dbdac7dc901eea99ed3914d3f88be29ee7fec150
idamodar9 <damodarvakamulla9@gmail.com>
2024-08-04 22:49:41 +0530

    newfile

---
+++ Caps_small.java
@@ -0,0 +1,17 @@
+class Caps_small
+{
+	public static void main(String[]args)
+	{
+		char ch = 'z';
+	  	{
+			if(ch>=65 && ch<=90)
+			{
+			   System.out.println("Caps");
+			}
+			else
+			{
+			  System.out.println("Small");
+			}
+		}
+	}
+}
